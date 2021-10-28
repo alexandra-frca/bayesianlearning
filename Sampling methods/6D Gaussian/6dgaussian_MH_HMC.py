a47cc5e6865f4ce8ce39b62fd43b935cc1dcf5aa
alexandra-frca <74411041+alexandra-frca@users.noreply.github.com>
2021-10-28 13:08:27 +0100

    Add files via upload

--- Sampling methods/6D Gaussian/6dgaussian_MH_HMC.py
+++ Sampling methods/6D Gaussian/6dgaussian_MH_HMC.py
@@ -38,7 +38,9 @@ def multivariate_gaussian_log_gradient(x, mu, sigma):
     return(np.dot(sigma_inv,x_mu))
     
 def target(xs):
-    means = np.array([10,10,10,-10,-10,-10])
+    means = np.array([15,15,15,-15,-15,-15])
+    #means = np.array([20,20,20,-20,-20,-20])
+    #means = np.array([17.5,17.5,17.5,-17.5,-17.5,-17.5])
     Sigma = np.identity(dim)
     return multivariate_gaussian(xs, means, Sigma)
 
@@ -46,17 +48,23 @@ def target_U(xs):
     return(-np.log(target(xs)))
 
 def target_DU(xs):
-    means = np.array([10,10,10,-10,-10,-10])
+    means = np.array([15,15,15,-15,-15,-15])
+    #means = np.array([20,20,20,-20,-20,-20])
+    #means = np.array([17.5,17.5,17.5,-17.5,-17.5,-17.5])
     Sigma = np.identity(dim)
     return multivariate_gaussian_log_gradient(xs, means, Sigma)
 
+first_rwm = True
 def metropolis_hastings_step(point, sigma=0.2):
-    global dim, accepted, total
+    global dim, accepted, total, first_rwm
+    if first_rwm:
+      print("RWM: sigma = %.2f" % sigma)
+      first_rwm = False
+      
     Sigma = sigma*np.identity(dim)
     new_point = np.random.multivariate_normal(point, Sigma)
 
-    p = target(new_point)*multivariate_gaussian(point,new_point,Sigma)/ \
-        (target(point)*multivariate_gaussian(new_point,point,Sigma))
+    p = target(new_point)/target(point)
     a = min(1,p)
 
     total += 1
@@ -72,11 +80,11 @@ def metropolis_hastings_path(steps,start=[0,0,0,0,0,0]):
     path.append(np.array(start))
     for t in range(1,steps+1):
         path.append(metropolis_hastings_step(path[t-1]))
-    print("MH: %d%% particle acceptance rate. " % (100*accepted/total))
+    print("MH: %.1f%% particle acceptance rate. " % (100*accepted/total))
     accepted, total = 0, 0
     return path
     
-def U_gradient(point,autograd=True):
+def U_gradient(point,autograd=False):
     if not autograd:
         DU = target_DU(point)
     else:
@@ -100,7 +108,7 @@ def simulate_dynamics(initial_momentum, initial_point, L, eta):
             np.sum(initial_momentum**2)/2-np.sum(new_momentum**2)/2)
     return new_point, p
         
-def hamiltonian_MC_step(point, M=np.identity(dim), L=20, eta=0.05):
+def hamiltonian_MC_step(point, M=np.identity(dim), L=30, eta=0.05):
     global accepted, total
     
     initial_momentum = np.random.multivariate_normal([0,0,0,0,0,0], M)
@@ -119,12 +127,12 @@ def hamiltonian_MC_path(steps,start=[0.,0.,0.,0.,0.,0.]):
     path.append(np.array(start))
     for t in range(1,steps+1):
         path.append(hamiltonian_MC_step(path[t-1]))
-    print("HMC: %d%% particle acceptance rate. " % (100*accepted/total))
+    print("HMC: %.1f%% particle acceptance rate. " % (100*accepted/total))
     return path
 
 def colored_scatter(path):
     fig, axs = plt.subplots(3,figsize=(8,8))
-    fig.subplots_adjust(hspace=0.35)
+    #fig.subplots_adjust(hspace=0.35)
     
     l = len(path)
     colors = []
@@ -139,9 +147,9 @@ def colored_scatter(path):
             axs[j].set_ylabel(r"$x_%d$" % (2*j+2))
       
 def colored_scatters(paths, titles=None):
-    fig, axs = plt.subplots(3,len(paths),figsize=(8,8))
-    fig.subplots_adjust(hspace=0.3)
-    fig.subplots_adjust(wspace=0.3)
+    fig, axs = plt.subplots(len(paths),3,figsize=(24,16))
+    #fig.subplots_adjust(hspace=0.3)
+    #fig.subplots_adjust(wspace=0.3)
     
     for n,path in enumerate(paths):
         l = len(path)
@@ -151,20 +159,22 @@ def colored_scatters(paths, titles=None):
         
         for i, point in enumerate(path):
             for j in range(dim//2):
-                axs[j,n].scatter(point[j*2], point[j*2+1], marker='o',
-                            s=5,color=colors[i])
-                axs[j,n].set_xlabel(r"$x_%d$" % (2*j+1))
-                axs[j,n].set_ylabel(r"$x_%d$" % (2*j+2))
+                axs[n,j].scatter(point[j*2], point[j*2+1], marker='x' if i==0 else 'o',
+                            s=100,color=colors[i])
+                axs[n,j].set_xlabel(r"$x_%d$" % (2*j+1),size=20)
+                axs[n,j].set_ylabel(r"$x_%d$" % (2*j+2),size=20)
                 
         if (titles!=None):
-            axs[0,n].set_title(titles[n],fontsize=14,pad=15)
+          for j in range(dim//2):
+            ttl = titles[n] + (" (dimensions %d and %d)" % (2*j+1,2*j+2))
+            axs[n,j].set_title(ttl,fontsize=25,pad=15)
     
 def main():
-    steps=50
+    steps=1000
     MH_path = metropolis_hastings_path(steps)
     HMC_path = hamiltonian_MC_path(steps)
 
     colored_scatters([MH_path,HMC_path], 
-                     titles=["Metropolis-Hastings","Hamiltonian Monte Carlo"])
+                     titles=["RWM","HMC"])
     
 main()
